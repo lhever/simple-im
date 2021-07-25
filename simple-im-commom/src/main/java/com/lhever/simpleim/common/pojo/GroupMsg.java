34fe0ccb6e6099b9c2c0e9e762a4cfb1591c9d6d
lhever <879675643@qq.com>
2021-07-25 23:39:30 +0800

    add pojo

---
+++ simple-im-commom/src/main/java/com/lhever/simpleim/common/pojo/GroupMsg.java
@@ -0,0 +1,105 @@
+package com.lhever.simpleim.common.pojo;
+
+import java.util.Date;
+
+public class GroupMsg {
+    private String id;
+
+    private String createId;
+
+    private String groupId;
+
+    private String receiveIds;
+
+    private Integer type;
+
+    private String content;
+
+    private Integer sendStatus;
+
+    private Integer readStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(String createId) {
+        this.createId = createId == null ? null : createId.trim();
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId == null ? null : groupId.trim();
+    }
+
+    public String getReceiveIds() {
+        return receiveIds;
+    }
+
+    public void setReceiveIds(String receiveIds) {
+        this.receiveIds = receiveIds == null ? null : receiveIds.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getSendStatus() {
+        return sendStatus;
+    }
+
+    public void setSendStatus(Integer sendStatus) {
+        this.sendStatus = sendStatus;
+    }
+
+    public Integer getReadStatus() {
+        return readStatus;
+    }
+
+    public void setReadStatus(Integer readStatus) {
+        this.readStatus = readStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ simple-im-commom/src/main/java/com/lhever/simpleim/common/pojo/UserGroup.java
@@ -0,0 +1,55 @@
+package com.lhever.simpleim.common.pojo;
+
+import java.util.Date;
+
+public class UserGroup {
+    private String id;
+
+    private String groupId;
+
+    private String userId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId == null ? null : groupId.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
